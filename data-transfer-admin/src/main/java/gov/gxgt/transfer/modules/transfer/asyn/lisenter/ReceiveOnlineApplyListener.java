d78191f628da099d13e58a83dd4bbdc169e023f9
280378807@qq.com <3090717419lyj>
2020-11-09 12:44:09 +0800

    测试跑一跑

--- data-transfer-admin/src/main/java/gov/gxgt/transfer/modules/transfer/asyn/lisenter/ReceiveOnlineApplyListener.java
+++ data-transfer-admin/src/main/java/gov/gxgt/transfer/modules/transfer/asyn/lisenter/ReceiveOnlineApplyListener.java
@@ -85,7 +85,7 @@ public class ReceiveOnlineApplyListener {
                 eq("CANTONCODE", ythBdcEntity.getAreaCode()).eq("NAME", ythBdcEntity.getSpsx()).le("rownum", 1));
         String target = ythBdcEntity.getId() + "@" + ythBdcEntity.getAreaCode() + "@" + ythBdcEntity.getSpsx();
         if (inCatalogEntity == null) {
-            logger.error(ythBdcEntity.getId() + "：找不到相应的事项。");
+            logger.error(target + "：找不到相应的事项。");
             ythBdcEntity.setException(target + "：接口查询不到相应的事项。");
             ythBdcEntity.setState(-1);
             ythBdcService.updateById(ythBdcEntity);
@@ -94,7 +94,7 @@ public class ReceiveOnlineApplyListener {
         Map map = objectMapper.readValue(ythBdcEntity.getDataSb(), Map.class);
         map = getItemInfo(map, inCatalogEntity);
         if (map == null) {
-            logger.error(ythBdcEntity.getId() + "：找不到相应的事项。");
+            logger.error(target + "：找不到相应的事项。");
             ythBdcEntity.setException(target + "：接口查询不到相应的事项。");
             ythBdcEntity.setState(-1);
             ythBdcService.updateById(ythBdcEntity);
